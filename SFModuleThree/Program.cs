26942984dbddb516491ddeb58de3d8e93b9a1cf3
RizzieRu <rizzie@mail.ru>
2024-11-09 02:17:46 +0300

    Completed task 3.4.3

--- SFModuleThree/Program.cs
+++ SFModuleThree/Program.cs
@@ -1,17 +1,9 @@
 ﻿using SFModuleThree.Models;
 using System.Text;
 
-string myName;
-
-var user = new UserModel("Jane", 27, true, 37.5f);
-
-var stringBuilder = new StringBuilder();
-
-stringBuilder.AppendLine($"My name is {user.Name}");
-stringBuilder.AppendLine($"My age is {user.Age}");
-stringBuilder.AppendLine($"Do I have a pet? {user.IsHaveAPet}");
-stringBuilder.AppendLine($"My shoe size is {user.SizeOfFeet}");
-
-Console.WriteLine(stringBuilder.ToString());
-
-Console.ReadLine();
+enum Semaphore : int
+{
+	Red = 100,
+	Yellow = 200,
+	Green = 300
+}
