702a67a1c775935c0562a61212a881c14a602767
RizzieRu <rizzie@mail.ru>
2024-11-09 01:56:17 +0300

    Completed task 3.3.2

---
+++ SFModuleThree/Models/UserModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFModuleThree.Models
+{
+	internal class UserModel
+	{
+		public string Name { get; set; }
+
+		public int Age { get; set; }
+
+		public bool IsHaveAPet { get; set; }
+
+		public float SizeOfFeet { get; set; }
+
+		public UserModel(string name, int age, bool isHaveAPet, float sizeOfFeet)
+		{
+			Name = name;
+
+			Age = age;
+
+			IsHaveAPet = isHaveAPet;
+
+			SizeOfFeet = sizeOfFeet;
+		}
+	}
+}
--- SFModuleThree/Program.cs
+++ SFModuleThree/Program.cs
@@ -1,5 +1,17 @@
-﻿string myName;
+﻿using SFModuleThree.Models;
+using System.Text;
 
-Console.WriteLine("\u0023");
+string myName;
+
+var user = new UserModel("Jane", 27, true, 37.5f);
+
+var stringBuilder = new StringBuilder();
+
+stringBuilder.AppendLine($"My name is {user.Name}");
+stringBuilder.AppendLine($"My age is {user.Age}");
+stringBuilder.AppendLine($"Do I have a pet? {user.IsHaveAPet}");
+stringBuilder.AppendLine($"My shoe size is {user.SizeOfFeet}");
+
+Console.WriteLine(stringBuilder.ToString());
 
 Console.ReadLine();
